5c36819d409835e2a1f7668e906a84631fecfdb9
Daniel Bentes Perquim <lperquim@gmail.com>
2026-02-26 20:23:51 -0300

    fazendo o exercício 1

---
+++ lab-2/Exercicio01/src/main/java/ads/poo/Main.java
@@ -0,0 +1,16 @@
+package ads.poo;
+
+import java.lang.classfile.constantpool.IntegerEntry;
+
+public class Main {
+    static void main(){
+        int dia = Integer.parseInt(IO.readln("Entre com o dia: "));
+        IO.println("dia: " + dia);
+
+        String mes = IO.readln("Entre com o mês: ");
+        IO.println("mês: " + mes);
+
+        // TODO aprender a relacionar o dia com o mês
+        if (dia.equals(""))
+    }
+}
