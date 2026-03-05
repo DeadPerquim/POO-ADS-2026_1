70f672c9c88a1f8b9a1ec6aa41781a83b7bfdd0a
Daniel Bentes Perquim <lperquim@gmail.com>
2026-03-05 20:18:13 -0300

    Metodos e atributos

---
+++ aula-4/Exercicio1/src/main/java/ads/poo/Lampada.java
@@ -0,0 +1,30 @@
+package ads.poo;
+
+public class Lampada {
+
+    //Atributos
+    private boolean ligada;
+
+    //Métodos
+
+    boolean isLigada(){
+        return ligada;
+        }
+
+        boolean ligarDesligar(){
+        ligada = !ligada;
+        return ligada;
+        }
+
+
+//    boolean ligar(){
+//        ligada = true;
+//        return ligada;
+//    }
+//    boolean desligar(){
+//        ligada = false;
+//        return ligada;
+//    }
+
+
+}
