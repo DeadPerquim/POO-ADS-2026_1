ea0e0f329f9deca06daba4bfca6933d17cbc7af3
Daniel Bentes Perquim <lperquim@gmail.com>
2026-04-09 19:45:49 -0300

    adicionando exercicio pra fazer

---
+++ aula13/src/main/java/ads/poo/App.java
@@ -0,0 +1,24 @@
+public class App {
+        void menu(int opcao) {
+            }
+    }
+    static void main(String[] args){
+
+//         TODO: No construtor dessa classe já crie 4 livros e adicione no acervo
+
+        // Sistema para gestão de acervo de uma Livraria
+//      1 - Cadastrar livro
+//      2 - Listar todos os livros
+//      3 - Listar todos os livros publicados em um ano específico
+//      4 - Alterar dados de um livro
+//      5 - Sair
+
+
+
+
+
+
+
+
+    }
+
